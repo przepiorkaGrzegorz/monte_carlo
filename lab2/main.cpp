e04f2c22d7e5accc9af92d012449ab328aafc76e
Greg <g.przepiorka@interia.pl>
2023-03-07 00:37:40 +0100

    Napisany rozkład złożony.

---
+++ lab2/main.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+#include<random>
+#include<vector>
+#include<fstream>
+#include<string>
+
+
+double uniform() {
+    return rand()/(RAND_MAX + 1.0) ;
+}
+
+double fX(const double x) {
+    return 0.8 * (1 + x - pow(x, 3));
+}
+
+double FX(const double x) {
+    return 0.8 * (x + 0.5*x*x + 0.25*pow(x, 4));
+}
+
+
+void probDensity(std::vector<double>& vec, const int N, const double width) {
+
+    double dx = width/(N - 1);
+    for (int x = 0; x != N; ++x) vec[x] = fX(x * dx);
+}
+
+
+void composite(std::vector<double>& vec) {
+
+    const double g1 = 0.8;
+    double U1 = 0.0, U2 = 0.0;
+
+    for (auto &v : vec) {
+        U1 = uniform();
+        U2 = uniform();
+
+        if (U1 <= g1) v = U2;
+        else v = sqrt(1 - sqrt(1 - U2));
+    }
+}
+
+
+void saveToFile(const std::vector<double>& vec, const std::string filePath) {
+
+    std::ofstream file;
+    file.open(filePath);
+
+    for (auto &v: vec) {
+        file << " " << v << "\n";
+    }
+
+    file.close();
+
+}
+
+
+void printn(const std::vector<double>& vec, const int n) {
+
+    for (int i = 0; i != n; ++i) printf("%f, ", vec[i]);
+    printf("\n");
+}
+
+
+int main() {
+
+    const int N = 10E06, k = 10;
+    std::vector<double> fVec(N, 0.0), compVec(N, 0.0);
+
+    probDensity(fVec, N, 1.0);
+    composite(compVec);
+    saveToFile(compVec, "composite.txt");
+    saveToFile(fVec, "fX.txt");
+    
+
+    printf("OK\n");
+    return 0;
+}
