19d2fedba571659d442d052e86d41add99b70fac
Greg <g.przepiorka@interia.pl>
2023-03-11 19:51:25 +0100

    Update main.cpp

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -14,7 +14,7 @@ double fX(const double x) {
 }
 
 double FX(const double x) {
-    return 0.8 * (x + 0.5*x*x + 0.25*pow(x, 4));
+    return 0.8 * (x + 0.5*x*x - 0.25*pow(x, 4));
 }
 
 void probDensity(std::vector<double>& vec, const int N, const double width) {
@@ -49,7 +49,6 @@ void markovChain(std::vector<double>& vec, const int N, const double delta) {
     }
 }
 
-
 void rejection(std::vector<double>& vec) {
     double U1, G2;
 
@@ -65,7 +64,6 @@ void rejection(std::vector<double>& vec) {
     }
 }
 
-
 void saveToFile(const std::vector<double>& vec, const std::string filePath) {
     std::ofstream file;
     file.open(filePath);
@@ -73,29 +71,63 @@ void saveToFile(const std::vector<double>& vec, const std::string filePath) {
     for (auto &v: vec) {
         file << " " << v << "\n";
     }
-
     file.close();
 }
-
 
 void printn(const std::vector<double>& vec, const int n) {
     for (int i = 0; i != n; ++i) printf("%f, ", vec[i]);
     printf("\n");
 }
 
+void binVals(const std::vector<double>& vec, int* bins, const double width, const int k) {
+    double binSize = width / k;
+    
+    for(auto &v: vec) {
+        bins[(int)(v/binSize)]++;
+    }
+}
+
+void getBinsProb(double* tab, const int k) {
+    for (int i = 0; i != k; ++i) {
+        tab[i] = FX(0.1*(i + 1.0)) - FX(0.1*i);
+    }
+}
+
+void chi2(const std::vector<double>& vec, const int N, const int k) {
+    int bins[k]{};
+    double binsProb[k]{}, chiSqVal = 0.0;
+    binVals(vec, bins, 1.0, k);
+    getBinsProb(binsProb, k);
+
+    for (int i = 0; i != k; ++i) {
+        printf("%f \n", binsProb[i]);
+    }
+
+    for (int i = 0; i != k; ++i) {
+        chiSqVal += pow((bins[i] - binsProb[i] * N), 2) / 
+                    (binsProb[i] * N);
+    }
+
+    printf("\n");
+    printf("%f \n", sqrt(chiSqVal));
+}
+
 
 int main() {
-    const int N = 10E06;
+    const int N = 1E06, k = 10;
+    const double alpha = 0.05; 
     std::vector<double> fVec(N, 0.0), compVec(N, 0.0), markovVec(N, 0.0), rejectVec(N, 0.0);
 
     probDensity(fVec, N, 1.0);
+    // saveToFile(fVec, "fX.txt");
     // composite(compVec);
     // saveToFile(compVec, "composite.txt");
-    // saveToFile(fVec, "fX.txt");
-    markovChain(markovVec, N, 0.05);
-    saveToFile(markovVec, "markov.txt");
-    // rejection(rejectVec);
-    // saveToFile(rejectVec, "reject.txt");
+    // markovChain(markovVec, N, 0.05); // 0.5, 0.05
+    // saveToFile(markovVec, "markov.txt");
+    rejection(rejectVec);
+    saveToFile(rejectVec, "reject.txt");
+
+    chi2(rejectVec, N, k);
 
     printf("OK\n");
     return 0;
